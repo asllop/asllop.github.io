7c2f4b4a6713f14e308cf1641072530f1a2117ed
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-09-18 22:43:21 +0200

    Store theme selection

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function setNightTheme() {
     let element = document.body;
     element.classList.add("night_theme")
+    localStorage.setItem("theme", "night");
 }
 
 function setDayTheme() {
     let element = document.body;
     element.classList.remove("night_theme");
+    localStorage.setItem("theme", "day");
 }
