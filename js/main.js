afc1f7d626e5eea3b626c71ebac761782c40d423
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-09-19 09:38:42 +0200

    Add margins

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function setNightTheme() {
-    let element = document.body;
+    let element = document.getElementById("body_section");
     element.classList.add("night_theme")
     localStorage.setItem("theme", "night");
 }
 
 function setDayTheme() {
-    let element = document.body;
+    let element = document.getElementById("body_section");
     element.classList.remove("night_theme");
     localStorage.setItem("theme", "day");
 }
